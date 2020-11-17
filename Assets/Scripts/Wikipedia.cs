6145a17f406a37f6cc46f20267c4c74c4f445e2b
SmitsAnja <anja.smits@student.howest.be>
2020-11-17 15:35:20 +0100

    Added wikipedia script

---
+++ Assets/Scripts/Wikipedia.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Wikipedia : MonoBehaviour
+{
+	public Button yourButton;
+	public string URL;
+
+
+	void Start()
+	{
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+		
+	}
+
+	void TaskOnClick()
+	{
+		//Debug.Log("You have clicked the button!");
+		Application.OpenURL(URL.ToString());
+	}
+}
