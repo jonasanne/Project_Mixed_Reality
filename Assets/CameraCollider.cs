cfc0e46ff0f28126eb391198628df779ec21242e
SmitsAnja <anja.smits@student.howest.be>
2020-11-30 11:43:53 +0100

    Change text in main scene on collision with molecule model

---
+++ Assets/CameraCollider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraCollider : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider_)
+    {
+        if (collider_.gameObject.tag == "ARCamera")
+        {
+            Debug.Log("H2Oloaded");
+            Text myText = GameObject.Find("AR Session Origin/Canvas/Text").GetComponent<Text>();
+            myText.text = "H2OLoaded";
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        Debug.Log("H2Oremoved");
+    }
+}
