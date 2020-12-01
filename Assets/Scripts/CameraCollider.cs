0dae6bce28222d56ad64a5d866e52d5b0827d6d5
unknown <jonas.anne@outlook.com>
2020-12-01 16:09:59 +0100

    model wordt opgehaald op basis van lijst met modellen

--- Assets/Scripts/CameraCollider.cs
+++ Assets/Scripts/CameraCollider.cs
@@ -5,34 +5,48 @@ using UnityEngine.UI;
 
 public class CameraCollider : MonoBehaviour
 {
+    public DataScript d;
     private void OnTriggerEnter(Collider collider_)
     {
-        //var dataScript = GetComponent<Data>();
-        //var modelList = dataScript.Models;
+        d = GetComponent<DataScript>();
+        var modelList = d.Models;
+        //Debug.Log(d);
+        //Debug.Log(d.Models);
+        //foreach (Model i in modelList)
+        //{
+        //    Debug.Log(i.ShortName);
+        //}
+
+        //Debug.Log(modelList.Find(m => m.ShortName == "H2O").Name);
+
+        //Debug.Log(modelList);
 
         if (collider_.gameObject.tag == "ARCamera")
         {
-            Debug.Log(gameObject.tag); // laad tag in van de molecule
+
+            Model model = modelList.Find(m => m.ShortName == gameObject.tag);
+            //Debug.Log(gameObject.tag); // laad tag in van de molecule
+            var tagname = gameObject.tag;
+
 
             Debug.Log("MoleculeLoaded");
             //Text myText = GameObject.Find("AR Session Origin/Canvas/Text").GetComponent<Text>();
             //myText.text = "H2OLoaded";
 
             Text formule = GameObject.Find("AR Session Origin/Canvas/Formule").GetComponent<Text>();
-            //formule.text = modelList.Find(p => p.ShortName == );
-            formule.text = "H2O";
+            formule.text = model.ShortName;
 
             Text solidState = GameObject.Find("AR Session Origin/Canvas/Solid-State/Solid-State-info").GetComponent<Text>();
-            solidState.text = "under 0°C";
+            solidState.text = model.Solid;
 
             Text liquidState = GameObject.Find("AR Session Origin/Canvas/Liquid-State/Liquid-State-info").GetComponent<Text>();
-            liquidState.text = "0 to 100°C";
+            liquidState.text = model.Liquid;
 
             Text gasState = GameObject.Find("AR Session Origin/Canvas/Gas-State/Gas-State-info").GetComponent<Text>();
-            gasState.text = "above 100°C";
+            gasState.text = model.Gas;
 
             Text infoMolecule = GameObject.Find("AR Session Origin/Canvas/Info").GetComponent<Text>();
-            infoMolecule.text = "Water is a polar inorganic compound that is at room temperature …";
+            infoMolecule.text = model.Description.Substring(0,90) + "...";
 
             //WikiLink = "https://en.wikipedia.org/wiki/Water";
         }
--- Assets/Scripts/DataScript.cs
+++ Assets/Scripts/DataScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 
 
-public class Data : MonoBehaviour
+public class DataScript : MonoBehaviour
 {
 
     public List<Model> Models = new List<Model>()
@@ -18,7 +18,9 @@ public class Data : MonoBehaviour
             Description = "Water is an inorganic, transparent, tasteless, odorless, and nearly colorless chemical substance, which is the main constituent of Earth's hydrosphere and the fluids of all known living organisms." +
             " It is vital for all known forms of life, even though it provides no calories or organic nutrients." +
             " Its chemical formula is H2O, meaning that each of its molecules contains one oxygen and two hydrogen atoms, connected by covalent bonds.",
-            State = "Liquid",
+            Solid = "0°C",
+            Gas = "100°C",
+            Liquid = "0°C to 100°C",
             WikipediaLink = "https://en.wikipedia.org/wiki/Water"
         },
         new Model
@@ -27,7 +29,9 @@ public class Data : MonoBehaviour
             ShortName= "CO2",
             Description = "Carbon dioxide (chemical formula CO2) is a colorless gas with a density about 53% higher than that of dry air. Carbon dioxide molecules consist of a carbon atom covalently double bonded to two oxygen atoms. It occurs naturally in Earth's atmosphere as a trace gas." +
             " The current concentration is about 0.04% (412 ppm) by volume, having risen from pre-industrial levels of 280 ppm.[8]",
-            State = "Gas",
+            Solid = "-78.5°C",
+            Gas = "-78.4°C",
+            Liquid = "none",
             WikipediaLink = "https://en.wikipedia.org/wiki/Carbon_dioxide"
         },
         new Model
@@ -37,7 +41,9 @@ public class Data : MonoBehaviour
             Description = "Ammonia is a compound of nitrogen and hydrogen with the formula NH3. A stable binary hydride, and the simplest pnictogen hydride, ammonia is a colourless gas with a characteristic pungent smell." +
             " It is a common nitrogenous waste, particularly among aquatic organisms, and it contributes significantly to the nutritional needs of terrestrial organisms by serving as a precursor to food and fertilizers." +
             " Ammonia, either directly or indirectly, is also a building block for the synthesis of many pharmaceutical products and is used in many commercial cleaning products. It is mainly collected by downward displacement of both air and water.",
-            State = "Gas",
+            Solid = "-77.7°C",
+            Gas = "-33.3°C",
+            Liquid = "-77.7°C to -33.3°C",
             WikipediaLink = "https://en.wikipedia.org/wiki/Ammonia"
         },
         new Model
@@ -47,7 +53,9 @@ public class Data : MonoBehaviour
             Description = "Methane (US: /ˈmɛθeɪn/ or UK: /ˈmiːθeɪn/) is a chemical compound with the chemical formula CH4 (one atom of carbon and four atoms of hydrogen)." +
             " It is a group-14 hydride and the simplest alkane, and is the main constituent of natural gas." +
             " The relative abundance of methane on Earth makes it an economically attractive fuel, although capturing and storing it poses technical challenges due to its gaseous state under normal conditions for temperature and pressure.",
-            State = "Gas",
+            Solid = "-182.5°C",
+            Gas = "-161.5°C",
+            Liquid = "-182.5°C to -161.5°C",
             WikipediaLink = "https://en.wikipedia.org/wiki/Methane"
         },
         new Model
@@ -56,7 +64,9 @@ public class Data : MonoBehaviour
             ShortName= "SO2",
             Description = "Sulfur dioxide or sulphur dioxide (British English) is the chemical compound with the formula SO2. It is a toxic gas responsible for the smell of burnt matches." +
             " It is released naturally by volcanic activity and is produced as a by-product of copper extraction and the burning of fossil fuels contaminated with sulfur compounds.",
-            State = "Gas",
+            Solid = "-72°C",
+            Gas = "-10°C",
+            Liquid = "-72°C to -10°C",
             WikipediaLink = "https://en.wikipedia.org/wiki/Sulfur_dioxide"
         },
         new Model
@@ -65,7 +75,9 @@ public class Data : MonoBehaviour
             ShortName= "C2H5OH",
             Description = "Ethanol (also called ethyl alcohol, grain alcohol, drinking alcohol, spirits, or simply alcohol) is an organic chemical compound." +
             " It is a simple alcohol with the chemical formula C2H6O. Its formula can be also written as CH3−CH2−OH or C2H5OH (an ethyl group linked to a hydroxyl group), and is often abbreviated as EtOH. Ethanol is a volatile, flammable, colorless liquid with a slight characteristic odor.It is a psychoactive substance, recreational drug, and the active ingredient in alcoholic drinks.",
-            State = "liquid",
+            Solid = "-114.1°C",
+            Gas = "78.2°C",
+            Liquid = "-114.1°C to 78.2°C",
             WikipediaLink = "https://en.wikipedia.org/wiki/Ethanol"
         },
     };
@@ -89,13 +101,10 @@ public class Data : MonoBehaviour
 public class Model
 {
     public string Name { get; set; }
-
     public string ShortName { get; set; }
-
     public string Description { get; set; }
-    
-    public string State { get; set; }
-
+    public string Solid { get; set; }
+    public string Liquid { get; set; }
+    public string Gas { get; set; }
     public string WikipediaLink { get; set; }
-
 }
