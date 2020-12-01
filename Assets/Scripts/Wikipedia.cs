de05c808dd33f0651a6efba534b4cc5d3fc2aff0
unknown <jonas.anne@outlook.com>
2020-12-01 17:12:41 +0100

    wikipedia link button

--- Assets/Scripts/Wikipedia.cs
+++ Assets/Scripts/Wikipedia.cs
@@ -4,20 +4,11 @@ using System.Collections;
 
 public class Wikipedia : MonoBehaviour
 {
-	public Button yourButton;
 	public string URL;
 
-
-	void Start()
-	{
-		Button btn = yourButton.GetComponent<Button>();
-		btn.onClick.AddListener(TaskOnClick);
-		
-	}
-
-	void TaskOnClick()
-	{
-		//Debug.Log("You have clicked the button!");
-		Application.OpenURL(URL.ToString());
-	}
+	public void OpenUrl()
+    {
+        Debug.Log("Opening Url");
+        Application.OpenURL(URL);
+    }
 }
