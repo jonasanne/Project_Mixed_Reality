f378bb3226b8b853c9d367c39d7276785a2e4370
unknown <jonas.anne@outlook.com>
2020-12-04 14:18:38 +0100

    bij item kiezen in de lijst doorverwezen naar main met variable nog geen prefab aanwezig bij doorverwijzing

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    private string ChosenMolecule;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        ChosenMolecule = PlayerPrefs.GetString("ChosenMolecule");
+        Debug.Log(ChosenMolecule);
+        if(ChosenMolecule != "")
+        {
+            //load molecule
+            string PathToMol = "Prefabs/" + ChosenMolecule;
+            GameObject Molecule = (GameObject)Resources.Load(PathToMol);
+            Debug.Log(Molecule);
+            //GameObject MoleculeInstance = Instantiate(Molecule);
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
